120ccc0b754c79fbe83efdf7b8b76590d4c4336c
Scott Brown <scott_brown_jp@yahoo.com>
2019-06-15 09:08:49 -0400

    Update

--- Python-Challenge/PyPoll/PYHW_Part2.py
+++ Python-Challenge/PyPoll/PYHW_Part2.py
@@ -7,21 +7,14 @@ County = []
 Candidate = []
 UniqueCandidate = []
 Winner = []
-#Numeric Variable Declarations
-WinnerIndex = 0
-CandTotal1 = 0
-CandTotal2 = 0
-CandTotal3 = 0
-CandTotal4 = 0
-Total = 0
 
-#Macro to find the index for the max value of a list
+#Function to find the index for the max value of a list
 def Max_Index(x):
     max_val = max(x)
     max_idx = x.index(max_val)
     return max_idx
 
-#Macro to tally up a candidates percentage total using their index within the
+#Function to tally up a candidates percentage total using their index within the
 #UniqueCandidate List
 def CandidateTotal(y):
     CandIndex = UniqueCandidate[y]
@@ -29,7 +22,7 @@ def CandidateTotal(y):
     return CandCount
     
 #Procedure to read in csv file
-with open('election_data1.csv', 'r') as csvfile:
+with open('election_data.csv', 'r') as csvfile:
     csvreader = csv.reader(csvfile, delimiter = ',')
     csvheader = next(csvreader) #Skip header
     for row in csvreader:
@@ -41,9 +34,10 @@ with open('election_data1.csv', 'r') as csvfile:
         #This is done in three steps: First, set the Candidate list in the new list UniqueCandidate.
         #The set function will return the unique values but in a random order with each run so it must then
         #be sorted using the Candidate index.  Finally, it is then put back as a list for further evaluation.
-        UniqueCandidate = set(Candidate)
-        UniqueCandidate = sorted(set(Candidate), key=Candidate.index)
-        UniqueCandidate = list(UniqueCandidate)
+
+UniqueCandidate = set(Candidate)
+UniqueCandidate = sorted(set(Candidate), key=Candidate.index)
+UniqueCandidate = list(UniqueCandidate)
 
 #The len function will yield the total votes cast.
 Total = len(Voter_ID)
@@ -55,7 +49,7 @@ CandTotal2 = CandidateTotal(1)
 CandTotal3 = CandidateTotal(2)
 CandTotal4 = CandidateTotal(3)
 
-#Each candidates percentage total is put into a list where the Max_Index macro is run to find the index
+#Each candidates percentage total is put into a list where the Max_Index function is run to find the index
 #of the largest value for the winner.
 Winner = [CandTotal1, CandTotal2, CandTotal3, CandTotal4]
 WinnerIndex = Max_Index(Winner)
@@ -69,7 +63,7 @@ with open('PythonHW_Part2_Election_Results_SDB.csv', 'w') as datafile:
     print(f"Total Votes: {Total}", file = datafile)
     print("--------------------------------", file = datafile)
     #Each of these 4 rows output the candidates name using the UniqueCandidate list, their percentage total with the
-    #CandidateTotal macro (rounded to 3 decimal places), and uses Candidate.count(UniqueCandidate[]) to display the
+    #CandidateTotal function (rounded to 3 decimal places), and uses Candidate.count(UniqueCandidate[]) to display the
     #total amount of votes won by counting how many times the candidates name appears in the original Candidate list.
     print(f"{UniqueCandidate[0]}: {round(CandTotal1, 3)}% ({Candidate.count(UniqueCandidate[0])})", file = datafile)
     print(f"{UniqueCandidate[1]}: {round(CandTotal2, 3)}% ({Candidate.count(UniqueCandidate[1])})", file = datafile)
@@ -77,8 +71,8 @@ with open('PythonHW_Part2_Election_Results_SDB.csv', 'w') as datafile:
     print(f"{UniqueCandidate[3]}: {round(CandTotal4, 3)}% ({Candidate.count(UniqueCandidate[3])})", file = datafile)
     print("--------------------------------", file = datafile)
     #The winner is determined using the WinnerIndex number within the UniqueCandidate list.
-    print(f"And The Win is: {UniqueCandidate[WinnerIndex]}!", file = datafile)
-    print(f"Congratulations {UniqueCandidate[WinnerIndex]}!", file = datafile)
+    print(f"And The Winner is: {UniqueCandidate[WinnerIndex]}!", file = datafile)
+    print(f"   Congratulations {UniqueCandidate[WinnerIndex]}!", file = datafile)
     print("--------------------------------", file = datafile)
     print("", file = datafile)
 
@@ -90,7 +84,7 @@ print("--------------------------------")
 print(f"Total Votes: {Total}")
 print("--------------------------------")
 #Each of these 4 rows output the candidates name using the UniqueCandidate list, their percentage total with the
-#CandidateTotal macro (rounded to 3 decimal places), and uses Candidate.count(UniqueCandidate[]) to display the
+#CandidateTotal function (rounded to 3 decimal places), and uses Candidate.count(UniqueCandidate[]) to display the
 #total amount of votes won by counting how many times the candidates name appears in the original Candidate list.
 print(f"{UniqueCandidate[0]}: {round(CandTotal1, 3)}% ({Candidate.count(UniqueCandidate[0])})")
 print(f"{UniqueCandidate[1]}: {round(CandTotal2, 3)}% ({Candidate.count(UniqueCandidate[1])})")
@@ -98,7 +92,7 @@ print(f"{UniqueCandidate[2]}: {round(CandTotal3, 3)}% ({Candidate.count(UniqueCa
 print(f"{UniqueCandidate[3]}: {round(CandTotal4, 3)}% ({Candidate.count(UniqueCandidate[3])})")
 print("--------------------------------")
 #The winner is determined using the WinnerIndex number within the UniqueCandidate list.
-print(f"And The Win is: {UniqueCandidate[WinnerIndex]}!")
-print(f"Congratulations {UniqueCandidate[WinnerIndex]}!")
+print(f"And The Winner is: {UniqueCandidate[WinnerIndex]}!")
+print(f"   Congratulations {UniqueCandidate[WinnerIndex]}!")
 print("--------------------------------")
 print("")
